31a83989a3ba0329a152836e46c8408d73736fed
cuonghx <cuonghx@cuonghxs-MacBook-Pro.local>
2019-04-11 13:21:13 +0700

    B2 lesson 3 autolayout using storyboard

---
+++ b2/b2/Color.swift
@@ -0,0 +1,13 @@
+//
+//  Color.swift
+//  b2
+//
+//  Created by cuonghx on 4/11/19.
+//  Copyright © 2019 cuonghx. All rights reserved.
+//
+import UIKit
+
+struct Color {
+    static let orange = UIColor(red: 253/256, green: 140/256, blue: 52/256, alpha: 1)
+    static let gray = UIColor(red: 214/256, green: 214/256, blue: 214/256, alpha: 1)
+}
